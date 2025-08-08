b4a0070dd3d765ec46d434578b240429fb2f729e
Prasanna Mahat <159425811+prasannamahat7@users.noreply.github.com>
2025-08-08 10:33:02 +0545

    Design changed

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -26,5 +26,18 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('blogs/', include('blog.urls')),
     path('pages/', include('pages.urls')),
+    #cart
+    path('cart/', views.cart, name="cart"),
+    #orders
+    path('checkout/', views.checkout, name="checkout"),
+    path('my-orders/', views.my_orders, name="my_orders"),
+    path('order-complete/', views.order_complete, name="order_complete"),
+    path('place-order/', views.place_order, name="place_order"),
+    #user
+    path('dashboard/', views.user_dashboard, name="user_dashboard"),
+    path('login/', views.user_login, name="login"),
+    path('register/', views.user_register, name="register"),
+    path('change-password/', views.user_changepassword, name="change_password"),
+    path('edit-profile/', views.user_edit_profile, name="change_password"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- marketplace/views.py
+++ marketplace/views.py
@@ -12,5 +12,36 @@ def home(request):
     return render(request, 'home/home1.html', {
         'products': products,
         'blogs': blogs
+
         })
 
+def user_login(request):
+    return render(request, "user/login.html")
+
+
+def user_register(request):
+    return render(request, "user/register.html")
+
+def user_dashboard(request):
+    return render(request, "user/dashboard.html")
+
+def user_edit_profile(request):
+    return render(request, "user/edit_profile.html")
+
+def cart(request):
+    return render(request, "cart/cart.html")
+
+def user_changepassword(request):
+    return render(request, "user/change_password.html")
+
+def checkout(request):
+    return render(request, "orders/checkout.html")
+
+def my_orders(request):
+    return render(request, "orders/my_orders.html")
+
+def order_complete(request):
+    return render(request, "orders/order_complete.html")
+
+def place_order(request):
+    return render(request, "orders/place_order.html")
